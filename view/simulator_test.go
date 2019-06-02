ab57bcd0607992cc6f5b06daf590290c897cc546
Lang Martin <lang@hashicorp.com>
2019-06-02 13:24:54 -0400

    simulator_test map to view pointers for update in place

--- view/simulator_test.go
+++ view/simulator_test.go
@@ -9,27 +9,32 @@ import (
 )
 
 const (
-	rounds    = 10
-	peers     = 10
+	rounds    = 4
+	peers     = 3
 	failure   = 0.01
 	slow      = 0.05
 	mortality = 0.01
 )
 
-type nodes map[string]View
+type nodes map[string]*View
 
-func testPush(v View, ns nodes) {
+func testPush(v *View, ns nodes) {
 	p := v.SelectPeer()
 	b := v.Push()
+
 	if rand.Float32() > failure {
 		// might be dead & therefore missing
 		if peer, ok := ns[p.Addr]; ok {
 			peer.Select(b)
+
+			// if p.Addr == "n0" {
+			// 	pretty.Log(peer.Peer)
+			// }
 		}
 	}
 }
 
-func testKill(v View, ns nodes) bool {
+func testKill(v *View, ns nodes) bool {
 	if rand.Float32() < mortality {
 		delete(ns, v.Addr)
 		return true
@@ -39,18 +44,23 @@ func testKill(v View, ns nodes) bool {
 
 func TestSimulation(t *testing.T) {
 	nodes := make(nodes)
-	nodes["n0"] = NewView("n0", "n1")
+	node := NewView("n0", "n0")
+	nodes["n0"] = &node
 
 	// init
 	for i := 1; i < peers; i++ {
 		addr := fmt.Sprintf("n%d", i)
-		nodes[addr] = NewView(addr, "n0")
+		node = NewView(addr, "n0")
+		nodes[addr] = &node
 	}
 
 	// run
 	for i := 0; i < rounds; i++ {
-		fmt.Println("ROUND", i)
 		for _, p := range nodes {
+			// if p.Addr == "n0" {
+			// 	pretty.Log("ROUND", p)
+			// }
+
 			if testKill(p, nodes) {
 				continue
 			}
