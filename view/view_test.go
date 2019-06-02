f854b2eda7b8d6c82320e770f6ce655915dbf4ca
Lang Martin <lang@hashicorp.com>
2019-06-02 12:54:56 -0400

    view_test, select seems to work on a basic level

--- view/view_test.go
+++ view/view_test.go
@@ -2,6 +2,8 @@ package view
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestMessageEqual(t *testing.T) {
@@ -10,4 +12,20 @@ func TestMessageEqual(t *testing.T) {
 	if !m.Equal(n) {
 		t.Fail()
 	}
+}
+
+func TestSelect(t *testing.T) {
+	n0 := NewView("n0", "n0")
+	n1 := NewView("n1", "n1")
+
+	require.Equal(t, Buffer{Message{"n1", 0, 0}}, n1.Peer)
+
+	b := n0.Push()
+	n1.Select(b)
+
+	require.Equal(t, Buffer{
+		Message{"n1", 0, 0},
+		Message{"n0", 0, 0},
+	},
+		n1.Peer)
 }
