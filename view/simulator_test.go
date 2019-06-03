607675fe5e873d6fdd2e72933374c98d9312790b
Lang Martin <lang@hashicorp.com>
2019-06-03 15:42:12 -0400

    simulate pushpull

--- view/simulator_test.go
+++ view/simulator_test.go
@@ -8,27 +8,53 @@ import (
 )
 
 const (
-	rounds    = 3000
-	peers     = 300
+	rounds    = 60
+	peers     = 3000
 	failure   = 0.00
+	replyFail = 0.00
 	slow      = 0.00
 	mortality = 0.00
 )
 
+type hist map[int]int
+
+func fmtHist(h hist) {
+	ks := make([]int, 0)
+	for k, _ := range h {
+		ks = append(ks, k)
+	}
+	sort.Ints(ks)
+	for _, k := range ks {
+		fmt.Printf("%02d => %d\n", k, h[k])
+	}
+}
+
 type nodes map[string]*View
 
+// testPush implements the push only algorithm
 func testPush(v *View, ns nodes) {
 	p := v.SelectPeer()
 	b := v.Push()
-
 	if rand.Float32() > failure {
 		// might be dead & therefore missing
 		if peer, ok := ns[p.Addr]; ok {
 			peer.Select(b)
+		}
+	}
+}
 
-			// if v.Addr == "n0" && p.Addr != "n0" {
-			// 	pretty.Log("push to", b, "ended at", peer.Peer)
-			// }
+// testPushPull implements the push + pull algorithm
+func testPushPull(v *View, ns nodes) {
+	p := v.SelectPeer()
+	b := v.Push()
+	if rand.Float32() > failure {
+		// might be dead & therefore missing
+		if peer, ok := ns[p.Addr]; ok {
+			peer.Select(b)
+			r := peer.Push()
+			if rand.Float32() > replyFail {
+				v.Select(r)
+			}
 		}
 	}
 }
@@ -43,7 +69,7 @@ func testKill(v *View, ns nodes) bool {
 
 func TestSimulation(t *testing.T) {
 	nodes := make(nodes)
-	// rand.Seed(42)
+	rand.Seed(42)
 
 	boot := []string{"n0", "n1", "n2", "n3"}
 
@@ -60,29 +86,15 @@ func TestSimulation(t *testing.T) {
 			if testKill(p, nodes) {
 				continue
 			}
-			testPush(p, nodes)
+			testPushPull(p, nodes)
 		}
 	}
 
 	// report OutDegree
-	size := make(map[int]int)
+	size := make(hist)
 	for _, p := range nodes {
 		s := len(p.Peer)
 		size[s] = size[s] + 1
 	}
-	ks := make([]int, 0)
-	for k, _ := range size {
-		ks = append(ks, k)
-	}
-	sort.Ints(ks)
-	for _, k := range ks {
-		fmt.Printf("%02d => %d\n", k, size[k])
-	}
-
-	// report InDegree
-	// indeg := make(map[int]int)
-	// for _, p := range nodes {
-	// 	s := len(p.Peer)
-	// 	size[s] = size[s] + 1
-	// }
+	fmtHist(size)
 }
