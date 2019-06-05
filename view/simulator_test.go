e8ce33698f17b14d310b8568a9acf4e718b6ea1d
Lang Martin <lang@hashicorp.com>
2019-06-05 17:54:00 -0400

    simulator_test check for partitions

--- view/simulator_test.go
+++ view/simulator_test.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 const (
-	rounds    = 100
-	peers     = 10000
-	failure   = 0.10
-	replyFail = 0.10
+	rounds    = 45
+	peers     = 1025
+	failure   = 0.00
+	replyFail = 0.00
 	slow      = 0.00 // not implemented
-	mortality = 0.10 // per test
+	mortality = 0.00 // per test
 )
 
 type nodes map[string]*View
@@ -33,11 +35,11 @@ func testPush(v *View, ns nodes) {
 func testPushPull(v *View, ns nodes) {
 	p := v.SelectPeer()
 	b := v.Push()
-	if rand.Float32() < failure {
+	if rand.Float32() > failure {
 		if peer, ok := ns[p.Addr]; ok {
 			peer.Select(b)
 			r := peer.Push()
-			if rand.Float32() < replyFail {
+			if rand.Float32() > replyFail {
 				v.Select(r)
 			}
 		}
@@ -57,14 +59,12 @@ func testKill(v *View, ns nodes, morgue morgue, time int) bool {
 func TestSimulation(t *testing.T) {
 	nodes := make(nodes)
 	morgue := make(morgue)
-	// rand.Seed(427)
-
-	boot := []string{"n0", "n1", "n2", "n3"}
+	// rand.Seed(4)
 
 	// init
 	for i := 0; i < peers; i++ {
 		addr := fmt.Sprintf("n%d", i)
-		node := NewView(addr, boot[i%len(boot)])
+		node := NewView(addr, "n0")
 		nodes[addr] = &node
 	}
 
@@ -82,4 +82,7 @@ func TestSimulation(t *testing.T) {
 	fmt.Printf("alive: %d, dead: %d\n", len(nodes), len(morgue))
 	fmtHist(rptOut(nodes))
 	fmtHist(rptIn(nodes))
+
+	// test tests
+	require.False(t, isPartitioned(nodes), "partitioned")
 }
